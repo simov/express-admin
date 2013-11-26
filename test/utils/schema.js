9954a87c6604783ab7a8ebfd1652ecd004e1c420
simov <simeonvelichkov@gmail.com>
2013-11-27 01:55:37 +0200

    Removed unused tests from utils/schema

--- test/utils/schema.js
+++ test/utils/schema.js
@@ -9,13 +9,11 @@ var db = require('../../lib/utils/database'),
 describe('schema', function () {
     var schema = null;
     before(function (done) {
-        db.connect({user: 'liolio', password: 'karamba'}, function (err) {
+        db.connect({mysql:{database: 'express-admin-simple',
+                    user:'liolio', password:'karamba'}}, function (err) {
             if (err) return done(err);
-            db.use('express-admin-simple', function (err) {
-                if (err) return done(err);
-                schema = new Schema(db);
-                done();
-            });
+            schema = new Schema(db);
+            done();
         });
     });
 
@@ -45,33 +43,6 @@ describe('schema', function () {
             var tables = Object.keys(columns);
             tables.join().should.equal(
                 'item,property,purchase,recipe,recipe_ref,recipe_type,subtype,type,user');
-            done();
-        });
-    });
-    it('get schema references', function (done) {
-        schema.getReferences(function (err, ref) {
-            if (err) return done(err);
-            var tables = Object.keys(ref);
-            should.deepEqual(tables, ['property','purchase','recipe','recipe_ref','type']);
-            // may test column properties too
-            done();
-        });
-    });
-    it('get table indexes', function (done) {
-        schema.getIndexes('purchase', function (err, indexes) {
-            if (err) return done(err);
-            Object.keys(indexes).join()
-                .should.equal('PRIMARY,fk_purchase_user_idx,fk_purchase_item1_idx');
-            done();
-        });
-    });
-    it('get schema indexes', function (done) {
-        schema.getAllIndexes(function (err, indexes) {
-            if (err) return done(err);
-            var tables = Object.keys(indexes);
-            tables.join().should.equal(
-                'item,property,purchase,recipe,recipe_ref,recipe_type,subtype,type,user');
-            // may test column properties too
             done();
         });
     });
