29165e3c4eae7c3c45f8a7fa4c8ba8c9f2975304
Abir Ibrahem <abir@accessnow.org>
2013-12-17 12:58:28 +0100

    add list view search filter

---
+++ public/script.js
@@ -0,0 +1,10 @@
+$(document ).ready(function() {
+         $("#list_view").dataTable({
+                "iDisplayLength": 10,
+                "sPaginationType": "full_numbers",
+                "aoColumnDefs": [
+
+                ],
+            });
+
+});
