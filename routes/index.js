58530c1d6aad2247f25aa19b44b573e1ed078e47
simov <simeonvelichkov@gmail.com>
2014-04-09 15:38:53 +0300

    Missed to remove upload route inclusion

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,5 @@ exports.notfound = require('./404');
 exports.mainview = require('./mainview');
 exports.listview = require('./listview');
 exports.editview = require('./editview');
-exports.upload   = require('./upload');
 
 exports.render   = require('./render');
