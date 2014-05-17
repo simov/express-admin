d816c639114f75d42db58c761ae8f7a3ebc4970e
simov <simeonvelichkov@gmail.com>
2014-05-17 05:58:07 +0300

    More db/client tests

--- test/db/client.js
+++ test/db/client.js
@@ -4,6 +4,8 @@ var path = require('path'),
 var Client = require('../../lib/db/client'),
     sql = require('../../lib/sql/sql'),
     query = require('../../lib/sql/query');
+var sqlitedb = path.join(__dirname,
+    '../../../express-admin-examples/fixtures/sqlite-examples/db.sqlite');
 
 
 describe('client (db)', function () {
@@ -22,7 +24,8 @@ describe('client (db)', function () {
         });
     });
 
-    describe('mysql', function () {
+
+    describe('connect', function () {
         it('set global var for sql modules', function (done) {
             var client = new Client({mysql:{}});
 
@@ -36,22 +39,18 @@ describe('client (db)', function () {
             query.client.sqlite.should.equal(false);
             done();
         });
-        it('connects to database', function (done) {
+        it('mysql - connects to database', function (done) {
             var config = {mysql:
-                {database:'express-admin-simple', user:'liolio', password:'karamba'}};
+                {database:'express-admin-examples', user:'liolio', password:'karamba'}};
             var client = new Client(config);
             client.connect(config.mysql, function (err) {
                 if (err) return done(err);
-                client.config.schema.should.equal('express-admin-simple');
+                client.config.schema.should.equal('express-admin-examples');
                 client.connection.destroy();
                 done();
             });
         });
-        // .query() ...
-    });
-
-    describe('pg', function () {
-        it('connects to database / set public schema by default', function (done) {
+        it('pg - connects to database / set public schema by default', function (done) {
             var config = {pg:
                 {database:'express-admin-examples', user:'liolio', password:'karamba'}};
             var client = new Client(config);
@@ -62,7 +61,7 @@ describe('client (db)', function () {
                 done();
             });
         });
-        it('connects to database / set schema name through config', function (done) {
+        it('pg - connects to database / set schema name through config', function (done) {
             var config = {pg: {database:'express-admin-examples', schema:'express',
                                 user:'liolio', password:'karamba'}};
             var client = new Client(config);
@@ -73,11 +72,8 @@ describe('client (db)', function () {
                 done();
             });
         });
-    });
-
-    describe('sqlite', function () {
-        it('connects to database', function (done) {
-            var config = {sqlite:{database:path.join(__dirname, '../../../express-admin-examples/fixtures/sqlite-examples/db.sqlite')}};
+        it('sqlite - connects to database', function (done) {
+            var config = {sqlite:{database:sqlitedb}};
             var client = new Client(config);
             client.connect(config.sqlite, function (err) {
                 if (err) return done(err);
@@ -85,6 +81,218 @@ describe('client (db)', function () {
                 client.connection.close();
                 done();
             });
+        });
+    });
+
+
+    describe('query', function () {
+        it('mysql - execute query', function (done) {
+            var config = {mysql:
+                {database:'express-admin-examples', user:'liolio', password:'karamba'}};
+            var client = new Client(config);
+            client.connect(config.mysql, function (err) {
+                if (err) return done(err);
+                client.query('select * from `user`;', function (err, rows) {
+                    if (err) return done(err);
+                    rows.length.should.equal(3);
+                    client.connection.destroy();
+                    done();
+                });
+            });
+        });
+        it('pg - execute query', function (done) {
+            var config = {pg:
+                {database:'express-admin-examples', user:'liolio', password:'karamba'}};
+            var client = new Client(config);
+            client.connect(config.pg, function (err) {
+                if (err) return done(err);
+                client.query('select * from "user";', function (err, rows) {
+                    if (err) return done(err);
+                    rows.length.should.equal(3);
+                    client.connection.end();
+                    done();
+                });
+            });
+        });
+        it('sqlite - execute query', function (done) {
+            var config = {sqlite:{database:sqlitedb}};
+            var client = new Client(config);
+            client.connect(config.sqlite, function (err) {
+                if (err) return done(err);
+                client.query('select * from `user`;', function (err, rows) {
+                    if (err) return done(err);
+                    rows.length.should.equal(3);
+                    client.connection.close();
+                    done();
+                });
+            });
+        });
+        it.skip('pg - insert query', function (done) {
+            // insert
+            done(new Error('Not implemented!'));
+            // remove
+        });
+        it.skip('sqlite - insert query', function (done) {
+            // insert
+            done(new Error('Not implemented!'));
+            // remove
+        });
+    });
+
+    describe('mysql - column settings', function () {
+        var columns = null;
+
+        before(function (done) {
+            var config = {mysql:
+                {database:'express-admin-examples', user:'liolio', password:'karamba'}};
+            var client = new Client(config);
+            client.connect(config.mysql, function (err) {
+                if (err) return done(err);
+                var sql = query.replace('show-columns', 'controls', 'express-admin-examples');
+                client.query(sql, function (err, rows) {
+                    if (err) return done(err);
+
+                    columns = client.getColumnsInfo(rows);
+                    // console.log(JSON.stringify(columns, null, 4));
+                    client.connection.destroy();
+                    done();
+                });
+            });
+        });
+        it('set allow null', function (done) {
+            columns.id.allowNull.should.equal(false);
+            columns.text.allowNull.should.equal(true);
+            done();
+        });
+        it('set pk', function (done) {
+            columns.id.key.should.equal('pri');
+            columns.text.key.should.equal('');
+            done();
+        });
+        it.skip('set default value', function (done) {
+            done();
+        });
+    });
+
+    describe('pg - column settings', function () {
+        var columns = null;
+
+        before(function (done) {
+            var config = {pg:
+                {database:'express-admin-examples', user:'liolio', password:'karamba'}};
+            var client = new Client(config);
+            client.connect(config.pg, function (err) {
+                if (err) return done(err);
+                var sql = query.replace('show-columns', 'controls', 'public');
+                client.query(sql, function (err, rows) {
+                    if (err) return done(err);
+                    
+                    columns = client.getColumnsInfo(rows);
+                    // console.log(JSON.stringify(columns, null, 4));
+                    client.connection.end();
+                    done();
+                });
+            });
+        });
+        it('set allow null', function (done) {
+            columns.id.allowNull.should.equal(false);
+            columns.text.allowNull.should.equal(true);
+            done();
+        });
+        it('set pk', function (done) {
+            columns.id.key.should.equal('pri');
+            columns.text.key.should.equal('');
+            done();
+        });
+        it.skip('set default value', function (done) {
+            done();
+        });
+    });
+
+    describe('sqlite - column settings', function () {
+        var columns = null;
+
+        before(function (done) {
+            var config = {sqlite:{database:sqlitedb}};
+            var client = new Client(config);
+            client.connect(config.sqlite, function (err) {
+                if (err) return done(err);
+                var sql = query.replace('show-columns', 'controls', '');
+                client.query(sql, function (err, rows) {
+                    if (err) return done(err);
+                    
+                    columns = client.getColumnsInfo(rows);
+                    // console.log(JSON.stringify(columns, null, 4));
+                    client.connection.close();
+                    done();
+                });
+            });
+        });
+        it('set allow null', function (done) {
+            columns.text.allowNull.should.equal(true);
+            done();
+        });
+        it('set pk', function (done) {
+            columns.text.key.should.equal('');
+            done();
+        });
+        it.skip('set default value', function (done) {
+            done();
+        });
+    });
+
+
+    describe('pg - data types', function () {
+        var columns = null;
+
+        before(function (done) {
+            var config = {pg:
+                {database:'express-admin-examples', user:'liolio', password:'karamba'}};
+            var client = new Client(config);
+            client.connect(config.pg, function (err) {
+                if (err) return done(err);
+                var sql = query.replace('show-columns', 'controls', 'public');
+                client.query(sql, function (err, rows) {
+                    if (err) return done(err);
+                    
+                    columns = client.getColumnsInfo(rows);
+                    // console.log(JSON.stringify(columns, null, 4));
+                    client.connection.end();
+                    done();
+                });
+            });
+        });
+        it('double precision', function (done) {
+            columns.double.type.should.equal('double');
+            done();
+        });
+        it.skip('numeric', function (done) {
+            
+            done();
+        });
+        it.skip('numeric(0,0)', function (done) {
+            
+            done();
+        });
+        it('time without time zone', function (done) {
+            columns.time.type.should.equal('time');
+            done();
+        });
+        it('timestamp without time zone', function (done) {
+            columns.datetime.type.should.equal('timestamp');
+            done();
+        });
+        it.skip('character', function (done) {
+            
+            done();
+        });
+        it('character varying', function (done) {
+            columns.text.type.should.equal('varchar(45)');
+            done();
+        });
+        it('boolean', function (done) {
+            columns.boolean.type.should.equal('char');
+            done();
         });
     });
 });
