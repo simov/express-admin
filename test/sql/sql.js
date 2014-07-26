da4a01be25e49cb9921bc7788bfc2020adb16076
simov <simeonvelichkov@gmail.com>
2014-07-26 18:45:08 +0300

    Fix sql tests

--- test/sql/sql.js
+++ test/sql/sql.js
@@ -8,53 +8,54 @@ describe('sql (sql)', function () {
     });
     
     it('fullName', function (done) {
-        sql.fullName('table', 'id')
-            .should.equal('`table`.`id`');
+        sql.fullName({name: 'table'}, 'id').should.equal('`table`.`id`');
+        sql.fullName({table: 'table'}, 'id').should.equal('`table`.`id`');
+        sql.fullName({alias: 'table'}, 'id').should.equal('`table`.`id`');
         done();
     });
 
     it('fullNames', function (done) {
         var columns = ['id', 'name', 'notes'];
-        sql.fullNames('table', columns).join()
+        sql.fullNames({name: 'table'}, columns).join()
             .should.equal('`table`.`id`,`table`.`name`,`table`.`notes`');
         done();
     });
 
     it('cast', function (done) {
-        sql.cast('table', ['firstname', 'lastname'])
+        sql.cast({name: 'table'}, ['firstname', 'lastname'])
             .should.equal('CAST(`table`.`firstname` AS CHAR),'+
                             'CAST(`table`.`lastname` AS CHAR)');
         done();
     });
 
     it('concat', function (done) {
-        sql.castConcat('table', ['firstname', 'lastname'], 'name')
+        sql.castConcat({name: 'table'}, ['firstname', 'lastname'], 'name')
             .should.equal("CONCAT_WS(' ',CAST(`table`.`firstname` AS CHAR),"+
                             "CAST(`table`.`lastname` AS CHAR)) AS `name`");
         done();
     });
     
     it('group', function (done) {
-        sql.group('table', ['firstname', 'lastname'], 'name')
+        sql.group({name: 'table'}, ['firstname', 'lastname'], 'name')
             .should.equal("GROUP_CONCAT(DISTINCT CONCAT_WS(' ',CAST(`table`.`firstname` AS CHAR),"+
                             "CAST(`table`.`lastname` AS CHAR))) AS `name`");
         done();
     });
 
     it('groupby', function (done) {
-        sql.groupby('table', 'column')
+        sql.groupby({name: 'table'}, 'column')
             .should.equal(' GROUP BY `table`.`column` ');
         done();
     });
 
     it('order', function (done) {
-        sql.order('table', {column1: 'asc', column2: 'desc'}).join()
+        sql.order({name: 'table'}, {column1: 'asc', column2: 'desc'}).join()
             .should.equal('`table`.`column1` asc,`table`.`column2` desc');
         done();
     });
 
     it('joins', function (done) {
-        sql.joins('table', 'fk', 'refTable', 'pk')
+        sql.joins({name: 'table'}, 'fk', {table: 'refTable'}, 'pk')
             .should.equal(' LEFT JOIN `refTable`  ON `table`.`fk` = `refTable`.`pk`');
         done();
     });
